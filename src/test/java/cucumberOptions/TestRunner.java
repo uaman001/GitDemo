49d98a5b9e3d9f3c54b35ce589c2c9b27fc03fb8
uaman001 <amanulmas@gmail.com>
2018-09-16 23:15:45 -0400

    time2

--- src/test/java/cucumberOptions/TestRunner.java
+++ src/test/java/cucumberOptions/TestRunner.java
@@ -9,7 +9,7 @@ import cucumber.api.junit.Cucumber;
 @CucumberOptions(features = "src/test/java/features", //---features = "src/test/java/features/Login.feature" -- for one feature only
 						glue = "stepDefinition" , 
 						tags = "@PortalTest",  //-- tags = "@SmokeTest,@RegTest,@Sanity", //--- optional
-						dryRun=true,  //---optional, checks only whether the TCs are mapped with stepDefinitions, but will skip TCs
+//						dryRun=true,  //---optional, checks only whether the TCs are mapped with stepDefinitions, but will skip TCs
 						monochrome = true, //---optional, gives readable output in Console report
 						strict = true,  //---optional, shows unmapped  
 						plugin = {"pretty",  //---optional, for readable report
